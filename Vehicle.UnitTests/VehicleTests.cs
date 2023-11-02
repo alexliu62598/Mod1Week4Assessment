7758996b8437a08faed65dc104df320a5b7ccc42
alexliu62598 <aliu23@turing.edu>
2023-11-02 09:53:09 -0700

    Completed Exercise

--- Vehicle.UnitTests/VehicleTests.cs
+++ Vehicle.UnitTests/VehicleTests.cs
@@ -20,9 +20,57 @@ namespace Vehicle.UnitTests
 
             var expected = "This Blue vehicle has 2 wheels and has driven 0 miles";
 
-
+            Assert.Equal(expected, andysBike.Summary());
         }
 
         // Add more tests here!
+
+        [Fact]
+        public void NumberOfWheelsProperty_CanBeRetrieved()
+        {
+            var alexsCar = new Vehicle(4, "Black");
+
+            Assert.Equal(4, alexsCar.NumberOfWheels);
+        }
+
+        [Fact]
+        public void ColorProperty_CanBeRetrieved()
+        {
+            var alexsCar = new Vehicle(4, "Black");
+
+            Assert.Equal("Black", alexsCar.Color);
+        }
+
+        [Fact]
+        public void MilesDrivenProperty_CanBeRetrieved()
+        {
+            var alexsCar = new Vehicle(4, "Black");
+
+            Assert.Equal(0, alexsCar.MilesDriven);
+        }
+
+
+        [Fact]
+        public void Drive_AddstoMilesDriven()
+        {
+            var alexsCar = new Vehicle(4, "Black");
+
+            var expected = 5;
+
+            alexsCar.Drive();
+
+            Assert.Equal(expected, alexsCar.MilesDriven);
+
+        }
+
+        [Fact]
+        public void Paint_ReassignsColorProperty()
+        {
+            var alexsCar = new Vehicle(4, "Black");
+
+            alexsCar.Paint("Brown");
+
+            Assert.Equal("Brown", alexsCar.Color);
+        }
     }
 }
